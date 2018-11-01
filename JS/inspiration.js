a3d2c2d0d296cf0d2044207cf34787d48a087c2a
GuiDevloper <guicorrea384@gmail.com>
2018-10-31 21:12:54 -0300

    onResize do dynamic update

--- JS/inspiration.js
+++ JS/inspiration.js
@@ -28,6 +28,15 @@ var Ajax = {
 		return $this.readyState == 4 && $this.status == 200;
 	}
 };
+// width of screen
+var widthBody = function() {
+  return window.innerWidth ||
+  document.documentElement.clientWidth ||
+  document.body.clientWidth;
+};
+window.addEventListener('resize', function() {
+  loadGrid();
+});
 document.addEventListener('DOMContentLoaded', function(){
   var imgx = Ajax.send("imgs.json", "GET");
   imgx.onreadystatechange = function() {
@@ -38,18 +47,18 @@ document.addEventListener('DOMContentLoaded', function(){
         // percorre imagem da coluna
         for(var item of Object.values(col)){
           // adiciona ao DOM
-          getByClass("row")[0].innerHTML +=
+          getByClass("grid")[0].innerHTML +=
             `<div class='grid-item'>
               <div class='card'>
-                <div class='card-image' style="${item[2]}">
+                <div class='image' style="${item[2]}">
                   <img class='responsive-img' size='100%' src='${item[1]}'>
                   <a target="_blank" href="${item[0]}">Ver no Pinterest</a>
                 </div>
                 <div class='frase'>
-                  <div class='card-content flow-toggle'>
+                  <div class='content flow-toggle'>
                     <p>...</p>
                   </div>
-                  <div class='card-action'></div>
+                  <div class='action'></div>
                 </div>
               </div>
             </div>`;
@@ -72,12 +81,12 @@ document.addEventListener('DOMContentLoaded', function(){
           var frase = sorted[0];
           var autor = sorted[1];
           var link = `
-          <a class='red-text' href='https://www.google.com.br/search?q=${autor}'>
+          <a class='red-text'  target="_blank" href='https://www.google.com.br/search?q=${autor}'>
             ${autor}
           </a>`;
           // trocando texto do elemento pela frase
           getByTag("p", elem)[0].innerHTML = frase;
-          getByClass("card-action", elem)[0].innerHTML = link;
+          getByClass("action", elem)[0].innerHTML = link;
         });
         isLoaded();
       }
@@ -99,31 +108,49 @@ document.addEventListener('DOMContentLoaded', function(){
 
   var i = 0;
   var icones = [getById('maiszoom'), getById('menoszoom')];
-  var percs = ['25%', '33%', '50%'];
+  var percs = [25, 33, 50, 100];
   [...icones].forEach(function(elem) {
     elem.addEventListener('click', function(){
       var mais = elem == icones[0];
       i = mais ? ++i : --i;
-      i = i < 1 ? 0 : (i > 1 ? 2 : i);
-      setStyle(getByClass("grid-item"), "width: " + percs[i]);
+      i = i < 1 ? 0 : (i > 2 ? 3 : i);
+      setStyle(getByClass("grid-item"), `width: ${percs[i]}%`);
+      // Reload positions
+      loadGrid([i == 0 ? 3 : (i == 1 ? 2 : (i == 2 ? 1 : 0))+1, percs[i]]);
     });
   });
 
   var $container = getByClass("grid")[0];
   function isLoaded() {
     imagesLoaded($container, function() {
-      var grids = getByClass("grid-item", $container);
-      for (var i = 0, j = 0; i <= 44; i++) {
-        j = j < 3 ? ++j : 0;
-        setStyle([grids[i]],
-          `top: ${(i > 3 ?
-            parseInt(grids[i-4].style.top.replace('px', '')) +
-              parseInt(grids[i-4].offsetHeight) : 0)+"px"};
-          left: ${j*25 + '%'};`);
-      }
+      loadGrid();
       $container.style.opacity = '1';
     });
   }
+  // recarrega cards em grid
+  window.loadGrid = function(preWid) {
+    var grids = getByClass("grid-item", $container);
+    var width = widthBody();
+    // recebe vezes e % baseado no tamanho da tela
+    var cWid = preWid ? preWid : (width > 1000 ? [4, 25] : (
+      width > 769 ? [3, 33] :
+        (width > 381 ? [2, 50] : [1, 100])
+    ));
+    // muda tamanho da fonte caso necessário
+    setStyle(getByTag("body"),
+      `font-size: ${width < 381 ? 0.9 : 1.2 }rem;`
+    );
+    for (var i = 0, j = 0; i <= 44; i++) {
+      // usa armazenados, pega anteriores
+      setStyle([grids[i]],
+        `top: ${(i > (cWid[0]-1) ?
+          parseInt(grids[i - cWid[0]].style.top.replace('px', '')) +
+          parseInt(grids[i - cWid[0]].offsetHeight) : 0) + "px"};
+          left: ${j * cWid[1] + '%'};`);
+      // ++ se vezes atual for menor que o vezes armazenado
+      j = j < (cWid[0]-1) ? ++j : 0;
+    }
+  };
   function toggle(elem, clas) {
     elem = getByClass(elem);
     for (var el of [...elem]) {
@@ -132,7 +159,7 @@ document.addEventListener('DOMContentLoaded', function(){
   }
   var third = getByClass('third')[0];
   third.addEventListener('click', function(){
-    toggle('card-content', 'dark');
-    toggle('card-action', 'dark');
+    toggle('content', 'dark');
+    toggle('action', 'dark');
   });
 });
